783948c8811b776d0a9cf7d6d7dbd9ea41ef026a
castledragon <starlight1532@gmail.com>
2024-09-12 22:29:17 +0900

    review Create Create

--- FIB_Query/etcQuery.sql
+++ FIB_Query/etcQuery.sql
@@ -7,36 +7,58 @@ select * from user;
 
 select * from bookmark;
 
+truncate bookmark;
 -- alter table bookmark modify column id varchar(100) not null;
 
-truncate bookmark;
+select * from product;
+
+select product_code,sellcount from product where protype=1 order by sellcount;
+ -- update product set sellcount = sellcount *2 + 102 where sellcount<=100;
+
 
 describe review;
-select * from review;
 
-select * from product;
-select product_code,sellcount from product where protype=1 order by sellcount;
-update product set sellcount = sellcount *2 + 102 where sellcount<=100;
+select product_code, count(*) as rvc from review 
+group by product_code
+order by rvc;
+
+select * from review where product_code = 31;
 
 delimiter //
 create procedure temp_insert_review ()
 begin
 	declare pcode int default 1;
-    declare contents varchar(30) default '리뷰';
-    declare tempid varchar(30) default concat('test', pcode ,'@naver.com');
+    declare contents varchar(30);
+    declare tempid varchar(30);
     declare rvidx int;
     declare sellamount int;
+    declare random_value int;
+    declare max_reviews int;
     
 	while pcode <=53 do
 		select sellcount into sellamount from product where product_code = pcode;
 		set rvidx = 1;
+        SET random_value = FLOOR(10 + (RAND() * (347 - 10 + 1)));
+		SET max_reviews = LEAST(floor(sellamount / 2), random_value);
+       
         
-			while rvidx <= floor(sellamount/2 + floor(1+(rand()*10))) do
+            while rvidx <= max_reviews do
+				set contents = concat('리뷰',rvidx);
+				set tempid = concat('test', pcode, rvidx * floor(rand()*35) ,'@naver.com');
+                
 				INSERT INTO review (product_code, id, star_count, regdate, content, image)
 				VALUES (pcode, tempid, floor(1+(rand()*5)), now(), contents, null);
+                
 				set rvidx = rvidx+1;
+                
 			end while;
             
+		update product set viewcount = rvidx where product_code = pcode;
+        
+        update product 
+        set gradeavg = (select avg(star_count) from review where product_code =pcode)
+        where product_code = pcode;
+        
 		set pcode = pcode +1;
     
     end while;
